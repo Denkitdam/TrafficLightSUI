86ac01dd4aed6afd8ae391c14011fbbf3a46668c
denkitdam <cryptoden145@gmail.com>
2023-06-08 19:09:28 +0300

    Traffic light circle created

---
+++ TrafficLightSUI/TrafficLightSignal.swift
@@ -0,0 +1,26 @@
+//
+//  TrafficLightSignal.swift
+//  TrafficLightSUI
+//
+//  Created by Denis Kitaev on 08.06.2023.
+//
+
+import SwiftUI
+
+struct TrafficLightSignal: View {
+    let color: Color
+    
+    var body: some View {
+        Circle()
+            .frame(width: 200, height: 200)
+            .foregroundColor(color)
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+        
+    }
+}
+
+struct TraficLightSignal_Previews: PreviewProvider {
+    static var previews: some View {
+        TrafficLightSignal(color: .red)
+    }
+}
